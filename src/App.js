ca95e04c83533291fdac844e4be316f7dc6e6413
Paula0397 <paula230397@gmail.com>
2023-01-26 22:25:59 -0500

    peopleStarwars

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { getCharacter, getPeople } from "./api/people";
+import "./App.css"
+
 
 function App() {
+
+  const[people, setPeople] = useState([])
+  const [currentCharacter, setCurrentCharacter] = useState(1)
+  const [details, setDetails] = useState({})
+  const [errorState, setErrorState] = useState({hasError: false})
+  
+
+  useEffect(() => {
+    getPeople()
+    .then((data) => setPeople(data.results))
+    .catch(handleError)
+  }, [])
+
+  useEffect (() => {
+    getCharacter(currentCharacter).then( setDetails).catch(handleError)
+  }, [currentCharacter])
+
+  const handleError = (err) => {
+    setErrorState({hasError: true, message: err.message })
+  }
+
+  const ShowDetails = (character) =>{
+    const id = Number (character.url.split('/').slice(-2)[0])
+    setCurrentCharacter(id)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <ul>
+      {errorState.hasError && <div> {errorState.message} </div>}
+      {people.map((character) =>(
+        <li key={character.name} onClick={() => ShowDetails(character)}>
+          {character.name}
+        </li> 
+      ))}
+    </ul>
+    {details &&(
+      <aside>
+        <h1> {details.name} </h1>
+        <ul>
+          <li>height: {details.height} </li>
+          <li>mass: {details.mass} </li>
+          <li>year of birth: {details.birth_year} </li>
+        </ul>
+      </aside>
+    )}
     </div>
-  );
+  ); 
 }
 
 export default App;
